570a4336c397936704a0b1399f56591da3118e59
dolprat <dolpratxyz@gmail.com>
2024-03-23 15:38:35 +0700

    first commit

---
+++ Airport.py
@@ -0,0 +1,7 @@
+class Airport:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+def speak():
+    print("hello world.")
---
+++ main.py
@@ -0,0 +1,7 @@
+import Airport
+
+# a1 = Airport("air asia", 100)
+
+
+# print(a1.name)
+Airport.speak()
