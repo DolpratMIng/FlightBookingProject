0ce49b4774be0323925c6579a5add9e798195e60
Pantorn <longpantorn@gmail.com>
2024-03-24 14:00:20 +0700

    1 push airport

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import Airport
+from Airport import *
+isTrue = True
+AirList = ["Hatyai","Bangkok"]
+while(isTrue):
+    def start(self):
+        for i in self.AirList:
+            print(i,end=" ")
+        print()
+    date = input("enter you date in dd/mm/yy format: ")
+    fromAir = input("from what airport do you wanna go?: ")
+    toAir = input("to what airport you wanna go?: ")
+    airport = Airport(fromAir,toAir,date)
 
-# a1 = Airport("air asia", 100)
+    if(fromAir and toAir in AirList):
+        airport.toString()
+    else:
+        print('there is no data of this')
 
-
-# print(a1.name)
-Airport.speak()
