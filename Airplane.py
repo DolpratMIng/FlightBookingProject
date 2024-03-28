d13551ab6326ef7e2cf1d4019b4a11dd99e27f31
dolprat <dolpratxyz@gmail.com>
2024-03-28 21:45:17 +0700

    fix

--- Airplane.py
+++ Airplane.py
@@ -1,10 +1,15 @@
-import datetime
 class Airplane:
-    specific_time = datetime.time()
+
+    # Class attribute to keep track of the count
+    count = 0
     def __init__(self,airline, name, time, price):
         self.airline = airline
         self.name = name
         self.time = time
         self.price = price
+        # Increment the count and assign it to the 'number' attribute
+        Airplane.count += 1
+        self.number = Airplane.count
+
     def __str__(self):
-        return f"Airline: {self.airline}\nName: {self.name}\nTime: {self.time}\nPrice: {self.price}\n"
+        return f"Number: {self.number}\nAirline: {self.airline}\nName: {self.name}\nTime: {self.time}\nPrice: {self.price}THB\n"
--- mainAirplane.py
+++ mainAirplane.py
@@ -1,11 +1,17 @@
 from Airplane import *
+from Airport import *
 
 airplane1 = Airplane("Air Asia", "FD3114", "8:00 AM", 1520)
 airplane2 = Airplane("Vietjet", "VD2554", "1:00 PM", 1340)
 airplane3 = Airplane("Lion Air", "LD1550", "9:00 PM", 2000)
 
+
 listplane = [airplane1, airplane2, airplane3]
 
+airp1 = Airport()
+airp1.start()
+
+print(f"This is the available airlines on date: {airp1.date}")
 for i in listplane:
     print(i)
 
